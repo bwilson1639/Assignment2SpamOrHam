380a5fa8c0cbd1c0da8f5685b5894f7a8c700b87
Benjamin <bwilson1@ualberta.ca>
2021-11-18 11:24:21 -0700

    made plan and added the data set

---
+++ main.py
@@ -0,0 +1,21 @@
+#calculate training data for test
+#   for each fileName in test_Lemmantized
+#       save name to testList
+#   for each name in testList
+#       open file
+#       read file
+#           for each instance of word save it to a dictionary
+#       close file
+#   for each instance of spam
+#       calculate P
+#       save to dictionary
+#   for each instance of ham
+#       calculate P
+#       save to dictionary
+#   create 'probability_spam_words.txt'
+#   crate 'probability_ham_words.txt'
+#   for each word in dictionary
+#       save to .txt file
+
+
+#run test
